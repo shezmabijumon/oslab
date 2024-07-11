9d4a2f8612776f9cd69df1c52697a839ba8696b8
Shezma Bijumon <132686971+shezmabijumon@users.noreply.github.com>
2024-07-11 16:04:49 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -11,9 +11,9 @@ void gantt();
 
 void main(){
     int i;
-    printf("enter number of processes: ");
+    printf("enter the number of processes:");
     scanf("%d",&n);
-    printf("PID\tArrivalTime\tBurstTime\n");
+    printf("PID\tARRIVAL TIME\tBURST TIME\n");
     for(i=0;i<n;i++){
         scanf("%d%d%d",&p[i].pid,&p[i].at,&p[i].bt);
     }
@@ -21,46 +21,45 @@ void main(){
 }
 
 void FCFS(){
-    int i,j;
+    int i;
     int total_tat=0,total_wt=0;
 
-    p[0].wt=p[0].at;
+    p[0].ct=p[0].bt;
     for(i=1;i<n;i++){
-        p[i].wt=p[i-1].wt+p[i-1].bt;
-    }
-    for(i=0;i<n;i++){
-        p[i].ct=p[i].wt+p[i].bt+p[i].at;
+        p[i].ct=p[i-1].ct+p[i].bt;
     }
     for(i=0;i<n;i++){
         p[i].tat=p[i].ct-p[i].at;
     }
+    for(i=0;i<n;i++){
+        p[i].wt=p[i].tat-p[i].bt;
+    }
     //print table
-    
-    printf("PID\tAT\tBT\tCT\tTAT\tWT\n");
+
+    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\n\n");
     for(i=0;i<n;i++){
         printf("P%d\t%d\t%d\t%d\t%d\t%d\n",p[i].pid,p[i].at,p[i].bt,p[i].ct,p[i].tat,p[i].wt);
     }
-    
-    gantt();
     for(i=0;i<n;i++){
         total_tat+=p[i].tat;
         total_wt+=p[i].wt;
     }
     printf("\nAverage Waiting Time: %.2f\n", (float)total_wt / n);
     printf("\nAverage Turnaround Time: %.2f\n", (float)total_tat / n);
-}
 
+    gantt();
+}
 void gantt(){
     int i,j;
     int size=p[n-1].ct;
     printf("\nGANTT CHART\n\n");
     printf("+");
-    for ( i = 0; i < size+2; i++){
+    for(i=0;i<size+2;i++){
         printf("-");
     }
     printf("+\n");
     printf("|");
-    for ( i = 0; i < n; i++){
+    for(i=0;i<n;i++){
         printf("P%d",p[i].pid);
         for(j=0;j<p[i].bt-2;j++){
             printf(" ");
@@ -73,13 +72,11 @@ void gantt(){
         printf("-");
     }
     printf("+\n");
-    printf("%d", p[0].at);
-    for (i = 0; i < n-1; i++) {
+    printf("%d",p[0].at);
+    for (i = 0; i < n; i++) {
         for (j = 0; j < p[i].bt; j++)
             printf(" ");
         printf("%d", p[i].ct);
     }
-    for (j = 0; j < p[n-1].bt; j++)
-        printf(" ");
-    printf("%d\n",p[n-1].ct);
+    printf("\n");
 }
